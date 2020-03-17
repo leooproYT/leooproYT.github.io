0f7a60090640431dbef73bffda6bb326a44fb9be
leooproYT <61655935+leooproYT@users.noreply.github.com>
2020-03-17 20:01:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,26 +5,29 @@ let name = prompt('what is your name');
     let result = 'about' + ' ' + name;
     document.getElementById('nam').innerHTML = result;
 }
-nameAsk();
+nameAsk()
 
 function ageAsk(){
     let ag = prompt('how old are you');
     let rsl = 'i am' + ' ' + ag + ' ' + 'years' + ' ' + 'old';
     document.getElementById('age').innerHTML = rsl;
 }
-ageAsk();
 
+ageASk()
 function workAsk(){
     let work = prompt('what do you wok as');
     let workOpinion = prompt('what do you think about your work.(start with i think)');
     let workR = 'i work as a' + ' ' + work + ' ' + 'and' + ' ' + workOpinion;
     document.getElementById('wrk').innerHTML = workR;
 }
-workAsk();
+workAsk()
 
 function aboutMe(){
     let ab = prompt('write abit about your self (start with i)');
     let abR = ab;
     document.getElementById('about').innerHTML = abR;
 }
-aboutMe();
+
+aboutMe()
+
+
